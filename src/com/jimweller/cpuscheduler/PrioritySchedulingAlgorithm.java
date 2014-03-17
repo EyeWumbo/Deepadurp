b9981fc97312a748f4059db6ae4a91d6f96148d7
leo jiapei lin <jiapeil@uci.edu>
2014-03-16 21:18:42 -0700

    shit again fixed

--- src/com/jimweller/cpuscheduler/PrioritySchedulingAlgorithm.java
+++ src/com/jimweller/cpuscheduler/PrioritySchedulingAlgorithm.java
@@ -8,92 +8,112 @@
  */
 package com.jimweller.cpuscheduler;
 
+import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.PriorityQueue;
 
-public class PrioritySchedulingAlgorithm extends BaseSchedulingAlgorithm implements OptionallyPreemptiveSchedulingAlgorithm {
-    private boolean preemptive;
+public class PrioritySchedulingAlgorithm extends BaseSchedulingAlgorithm implements
+		OptionallyPreemptiveSchedulingAlgorithm {
+	private boolean preemptive;
 
-    private PriorityQueue<Process> processes = new PriorityQueue<Process>(16, new ProcessPriorityComparator());
-    private Process currentProcess;
-    
-    PrioritySchedulingAlgorithm(){
+	private ArrayList<Process> processes = new ArrayList<Process>();
+	private Process currentProcess;
+	private ProcessPriorityComparator comparator = new ProcessPriorityComparator();
+	PrioritySchedulingAlgorithm() {
 
-    }
+	}
 
-    
-    private class ProcessPriorityComparator implements Comparator<Process>{
+	private class ProcessPriorityComparator implements Comparator<Process> {
 
 		@Override
 		public int compare(Process a, Process b) {
 			// TODO Auto-generated method stub
-			if(a.priority > b.priority){
+			if (a.priority > b.priority) {
 				return 1;
 			}
-			else if(a.priority == b.priority){
+			else if (a.priority == b.priority) {
 				return 0;
 			}
 			return -1;
 		}
-    	
-    }
-    
-    /** Add the new job to the correct queue.*/
-    public void addJob(Process p){
-    	processes.add(p);
-    	super.addJob(p);
-    	if(currentProcess == null){
-    		currentProcess = processes.peek();
-    	}
-    }
-    
-    /** Returns true if the job was present and was removed. */
-    public boolean removeJob(Process p){
-    	super.removeJob(p);
-    	return processes.remove(p);
-    }
 
-    /** Transfer all the jobs in the queue of a SchedulingAlgorithm to another, such as
-	when switching to another algorithm in the GUI */
-    public void transferJobsTo(SchedulingAlgorithm otherAlg) {
-    	for(Process p : processes){
-    		this.removeJob(p);
-    		otherAlg.addJob(p);
-    		
-    	}
-    	processes.clear();
-    }
+	}
 
+	/** Add the new job to the correct queue. */
+	public void addJob(Process p) {
+		processes.add(p);
+		super.addJob(p);
+//		if (currentProcess == null) {
+//			currentProcess = processes.peek();
+//		}
+	}
 
-    /** Returns the next process that should be run by the CPU, null if none available.*/
-    public Process getNextJob(long currentTime){
-    	if(preemptive){
-    		currentProcess = processes.peek();
-    	}
-    	if(!preemptive && currentProcess.isFinished()){
-    		currentProcess = processes.peek();
-    	}
-    	if(currentProcess.isFinished()){
-    		currentProcess = null;
-    	}
-    	return currentProcess;
-    }
-    
-    public String getName(){
-	return "Single-queue Priority";
-    }
+	/** Returns true if the job was present and was removed. */
+	public boolean removeJob(Process p) {
+		super.removeJob(p);
+		return processes.remove(p);
+	}
 
-    /**
-     * @return Value of preemptive.
-     */
-    public boolean isPreemptive(){
-	return preemptive;
-    }
-    
-    /**
-     * @param v  Value to assign to preemptive.
-     */
-    public void setPreemptive(boolean  v){
-	preemptive = v;
-    }
+	/**
+	 * Transfer all the jobs in the queue of a SchedulingAlgorithm to another,
+	 * such as when switching to another algorithm in the GUI
+	 */
+	public void transferJobsTo(SchedulingAlgorithm otherAlg) {
+		for (Process p : processes) {
+			this.removeJob(p);
+			otherAlg.addJob(p);
+
+		}
+		processes.clear();
+	}
+
+	/**
+	 * Returns the next process that should be run by the CPU, null if none
+	 * available.
+	 */
+	public Process getNextJob(long currentTime) {
+		Process p1 = null;
+		Process p2 = null;
+		if(!isJobFinished() && !isPreemptive())
+			return activeJob;
+		p1 = processes.get(0);
+		for(int i = 0; i < processes.size(); ++i)
+		{
+			p2 = processes.get(i);
+			if(comparator.compare(p1, p2) < 0)
+			{
+				p1 = p2;
+			}
+		}
+		this.activeJob = p1;
+		return activeJob;
+//		if (preemptive) {
+//			currentProcess = processes.peek();
+//		}
+//		if (!preemptive && currentProcess.isFinished()) {
+//			currentProcess = processes.peek();
+//		}
+//		if (currentProcess.isFinished()) {
+//			currentProcess = null;
+//		}
+//		return currentProcess;
+	}
+
+	public String getName() {
+		return "Single-queue Priority";
+	}
+
+	/**
+	 * @return Value of preemptive.
+	 */
+	public boolean isPreemptive() {
+		return preemptive;
+	}
+
+	/**
+	 * @param v
+	 *            Value to assign to preemptive.
+	 */
+	public void setPreemptive(boolean v) {
+		preemptive = v;
+	}
 }
--- src/com/jimweller/cpuscheduler/SJFSchedulingAlgorithm.java
+++ src/com/jimweller/cpuscheduler/SJFSchedulingAlgorithm.java
@@ -10,92 +10,93 @@ package com.jimweller.cpuscheduler;
 
 import java.util.*;
 
-
 public class SJFSchedulingAlgorithm extends BaseSchedulingAlgorithm implements OptionallyPreemptiveSchedulingAlgorithm {
-    private boolean preemptive;
-    
-    private PriorityQueue<Process> processes = new PriorityQueue<Process>(16, new JobLengthPriorityComparator());
-    private Process currentProcess;
-    
-    SJFSchedulingAlgorithm(){
-    }
+	private boolean preemptive;
 
-    
-    private class JobLengthPriorityComparator implements Comparator<Process>{
+	private ArrayList<Process> processes = new ArrayList<Process>();
+	private JobLengthPriorityComparator comparator = new JobLengthPriorityComparator();
+
+	SJFSchedulingAlgorithm() {
+	}
+
+	private class JobLengthPriorityComparator implements Comparator<Process> {
 
 		@Override
 		public int compare(Process a, Process b) {
 			// TODO Auto-generated method stub
-			if(a.burst > b.burst){
+			if (a.burst > b.burst) {
 				return 1;
 			}
-			else if(a.burst == b.burst){
+			else if (a.burst == b.burst) {
 				return 0;
 			}
 			return -1;
 		}
-    	
-    }
-    
-    /** Add the new job to the correct queue.*/
-    public void addJob(Process p){
-    	super.addJob(p);
-    	processes.add(p);
-    	if(currentProcess == null){
-    		currentProcess = processes.peek();
-    	}
-//    	if(processes.peek().equals(p)){
-//    		if(currentProcess == null){
-//    			currentProcess = p;
-//    		}
-//    	}
-    }
-    
-    /** Returns true if the job was present and was removed. */
-    public boolean removeJob(Process p){
-    	super.removeJob(p);
-    	return processes.remove(p);
-    }
 
-    /** Transfer all the jobs in the queue of a SchedulingAlgorithm to another, such as
-	when switching to another algorithm in the GUI */
-    public void transferJobsTo(SchedulingAlgorithm otherAlg) {
-    	for(Process p : processes){
-    		this.removeJob(p);
-    		otherAlg.addJob(p);
-    	}
-    	processes.clear();
-    }
+	}
 
-    /** Returns the next process that should be run by the CPU, null if none available.*/
-    public Process getNextJob(long currentTime){
-    	if(preemptive){
-    		currentProcess = processes.peek();
-    	}
-    	else if(!preemptive && currentProcess.isFinished()){
-    		currentProcess = processes.peek();
-    	}
-    	if(currentProcess.isFinished()){
-    		currentProcess = null;
-    	}
-    	return currentProcess;
-    }
-    
-    public String getName(){
-	return "Shortest job first";
-    }
+	/** Add the new job to the correct queue. */
+	public void addJob(Process p) {
+		super.addJob(p);
+		processes.add(p);
+	}
 
-    /**
-     * @return Value of preemptive.
-     */
-    public boolean isPreemptive(){
-	return preemptive;
-    }
-    
-    /**
-     * @param v  Value to assign to preemptive.
-     */
-    public void setPreemptive(boolean  v){
-	preemptive = v;
-    }
+	/** Returns true if the job was present and was removed. */
+	public boolean removeJob(Process p) {
+		super.removeJob(p);
+		return processes.remove(p);
+	}
+
+	/**
+	 * Transfer all the jobs in the queue of a SchedulingAlgorithm to another,
+	 * such as when switching to another algorithm in the GUI
+	 */
+	public void transferJobsTo(SchedulingAlgorithm otherAlg) {
+		for (Process p : processes) {
+			this.removeJob(p);
+			otherAlg.addJob(p);
+		}
+		processes.clear();
+	}
+
+	/**
+	 * Returns the next process that should be run by the CPU, null if none
+	 * available.
+	 */
+	public Process getNextJob(long currentTime) {
+		Process p1 = null;
+		Process p2 = null;
+		if(!isJobFinished() && !isPreemptive())
+			return activeJob;
+		p1 = processes.get(0);
+		for(int i = 0; i < processes.size(); ++i)
+		{
+			p2 = processes.get(i);
+			if(comparator.compare(p1, p2) > 0)
+			{
+				p1 = p2;
+			}
+		}
+		this.activeJob = p1;
+		return activeJob;
+	}
+
+	public String getName() {
+		return "Shortest job first";
+	}
+
+	/**
+	 * @return Value of preemptive.
+	 */
+	public boolean isPreemptive() {
+		return preemptive;
+	}
+
+	/**
+	 * @param v
+	 *            Value to assign to preemptive.
+	 */
+	public void setPreemptive(boolean v) {
+		preemptive = v;
+	}
 }
